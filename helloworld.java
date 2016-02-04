3274333c333a72c7025506acdf78d9774d3006b4
Christopher Hayes <cphayes@optonline.net>
2016-02-04 10:40:29 -0500

    v1.3

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class HelloWorld
 {
-	private static void main(String[] args)
+	private static void main(String[] args)//This is a test comment
 	{
 		System.out.println("Hello World!");//Print out Hello World!
 	}
